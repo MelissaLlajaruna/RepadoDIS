57e32100a24669ad471728bc859738bb26b63bac
Melissa Llajaruna <7903909@alumnos.ufv.es>
2023-07-04 23:03:57 +0200

    Proyecto creado

---
+++ RepasoExtraordinaria/src/main/java/DIS/RepasoExtraordinaria/RepasoExtraordinariaApplication.java
@@ -0,0 +1,13 @@
+package DIS.RepasoExtraordinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepasoExtraordinariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepasoExtraordinariaApplication.class, args);
+	}
+
+}
---
+++ RepasoExtraordinaria/src/test/java/DIS/RepasoExtraordinaria/RepasoExtraordinariaApplicationTests.java
@@ -0,0 +1,13 @@
+package DIS.RepasoExtraordinaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepasoExtraordinariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
